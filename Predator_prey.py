a1cf53b1b9dd094c1afa11dbec725bb8e19dcb43
Jack <Jack904>
2023-02-22 11:09:15 +0000

    got the numbers to check with true values

--- Predator_prey.py
+++ Predator_prey.py
@@ -11,7 +11,7 @@ def ode(t,y,a,b,d): #Keeping t in in case our ode reuires it
 
 
 def shooting(x,F,conds): #Finds the points and time period of the limit cycle
-    condition_1 = x[:len(x)-1] - scipy.integrate.solve_ivp(F,[0, x[len(x)-1]],x[:len(x)-1], args=conds, rtol = 1e-4).y[:,-1]
+    condition_1 = x[:len(x)-1] - scipy.integrate.solve_ivp(F,[0, x[len(x)-1]],x[:len(x)-1], args=conds, rtol = 1e-6).y[:,-1]
     condition_2 = F(0,x[:len(x)-1],*conds)[0] 
 
     return [*condition_1,condition_2] # THe * gets rid of the list within
--- test.py
+++ test.py
@@ -14,29 +14,26 @@ def test_ode(t,u,beta,o):
     du2_dt = u[0] + beta*u[1] + o*u[1]*(((u[1])**2) + ((u[0])**2))
     du3_dt = -u[2]
 
-    u1 = (math.sqrt(beta))*(math.cos(t+2*math.pi))
-    u2 = (math.sqrt(beta))*(math.sin(t+2*math.pi))
-
     return [du1_dt, du2_dt, du3_dt]
 
 def test_ode_sols(t,beta):
-    u1 = (math.sqrt(beta))*(math.cos(t+2*math.pi))
-    u2 = (math.sqrt(beta))*(math.sin(t+2*math.pi))
+    theta = 2*math.pi
+    u1 = (math.sqrt(beta))*(math.cos(t+theta))
+    u2 = (math.sqrt(beta))*(math.sin(t+theta))
     u3 = math.exp(-t)
 
-    return [u1, u2, u3]
+    return [u1, u2, u3, theta]
 
-solution = scipy.integrate.solve_ivp(test_ode,[0,30],[0.5,0.5,0.5],args = [beta,o],rtol = 1e-5)
+solution = scipy.integrate.solve_ivp(test_ode,[0,30],[1,0,1],args = [beta,o],rtol = 1e-5)
 #Plotting ODE
 
+initial_guess = [1,0,0,6]
 
-initial_guess = [0.8,-0.01,0,6]
 
-
-result = scipy.optimize.root(shooting, x0 = initial_guess, args = (test_ode, conds), tol = 1e-4)
+result = scipy.optimize.root(shooting, x0 = initial_guess, args = (test_ode, conds), tol = 1e-6)
 print(result.x)
-print(test_ode_sols(1,beta))
-
+print(test_ode_sols(4*math.pi,beta))
+print(np.isclose(result.x,test_ode_sols(4*math.pi,beta), rtol = 1e-4, atol = 1e-4))
 #Explicit Solutions
 
 #u1 = (beta^(1/2))*(math.cos(t+2*math.pi))
@@ -45,6 +42,6 @@ print(test_ode_sols(1,beta))
 plt.plot(solution.t,solution.y[0,:], label = 'u1')
 plt.plot(solution.t,solution.y[1,:], label = 'u2')
 plt.plot(solution.t,solution.y[2,:], label = 'u3')
-plt.plot(result.x[0],result.x[1], marker = 'o')
+plt.plot(0,result.x[1], marker = 'o')
 plt.legend()
 plt.show()
