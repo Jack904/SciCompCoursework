b31cd6017130e76191c90baa0bcdd4c3ecb925a1
Jack <Jack904>
2023-03-21 11:34:02 +0000

    Pseudo Continuation working, probably not for long

--- Numerical_cont.py
+++ Numerical_cont.py
@@ -9,18 +9,6 @@ def funct(x):
     return (x[1]**3) -x[1] + x[0]
 
 
-# def natural_parameter(f, x0, p0,p1, no_of_steps):
-#     solutions = np.array([x0])
-#     c_values = np.array([p0])
-#     c_range = np.linspace(p0,p1, no_of_steps)
-#     for i in range(0,len(c_range)-1):
-#         p = c_range[i]
-#         prediction = solutions[-1]
-#         result = scipy.optimize.root(f, x0 = prediction,args=(p))
-#         if result.success == True:
-#             solutions = np.append(solutions,result.x)
-#             c_values =  np.append(c_values,p)
-#     return c_values[1:], solutions[1:]
 def natural_parameter(ode, initial_point, p0, p1, no_of_steps):
     x0 = [p0,initial_point]
     
@@ -30,9 +18,8 @@ def natural_parameter(ode, initial_point, p0, p1, no_of_steps):
     for i in range(no_of_steps):
         x0[0] +=  h
         # x0[-1]-=0.25
-        myfunc = lambda u: np.concatenate(([ode([x0[0],u[1]])], [u[0] - x0[0]]))
+        myfunc = lambda u: np.concatenate(([ode([u[0],u[1]])], [u[0] - x0[0]]))
         result = scipy.optimize.root(myfunc, x0 = x0, tol = 1e-10)
-        print(result.message)
         
         x0[-1] = result.x[1]
         
@@ -43,19 +30,32 @@ def natural_parameter(ode, initial_point, p0, p1, no_of_steps):
             c_vals.append(x0[0])
     return [x_vals, c_vals]
 
-def psuedo_parameter(ode, initial_x,p0,p1, no_of_steps):
-    x0 = np.asarray(initial_x)
-    c_values = np.array(p0)
-    c_range = np.linspace(p0,p1, no_of_steps)
-    for i in range(no_of_steps):
-        secant = x0[i+1]-x0[i]
-        predictions = x0[-1] + secant
-
-        myfunc = lambda u: np.concatenate(([ode(u)], [np.dot((u - predictions[-1]),secant)]))
-        result = scipy.optimize.root(myfunc, x0 = x0[i])
-        np.append(x0,result.x[1])
+def psuedo_parameter(ode, initial_point,p0,p1, no_of_steps):
+    h = (abs(p0-p1))/no_of_steps
+    xi_minus_one = [p0,initial_point]
+    [y1, c1]  = natural_parameter(ode, initial_point,p0,p0+h,1)
+    xi = [p0+h, y1[1]]
+    x_vals = [initial_point, y1[1]]
+    c_vals = [p0, p0+h]
+    secant = np.zeros(2)
+    i =0
+    while xi[0] < p1:
+        i += 1
+        secant[0] = xi[0] - xi_minus_one[0]
+        secant[1] = xi[1] - xi_minus_one[1]
+        prediction = xi + secant
+        myfunc = lambda u: np.concatenate(([ode([u[0],u[1]])], [np.dot((u-prediction),secant)]))
+        result = scipy.optimize.root(myfunc, x0 = prediction, method = 'krylov')
+        xi_minus_one = xi
+        xi = [result.x[0], result.x[1]]
     
-    return [x0]
+        if result.success == True:
+            x_vals.append(result.x[1])
+            c_vals.append(result.x[0])
+
+        if i > no_of_steps:
+            break
+    return [c_vals, x_vals]
 
 
 def ode(t,y,a=1,b=2,d=3): #Keeping t in in case our ode reuires it
@@ -73,10 +73,12 @@ if __name__ == "__main__":
     # x,y = psuedo_parameter(funct,[(-2,1.52137971),(-1.995996,1.52070571)],-2,2,1000)
     x_true = np.linspace(-2,2,100)
     y_true = funct((0,-x_true))
-    x,y = natural_parameter(funct, 2, -2,2,1000)
-    print(x,y)
+    y,x = natural_parameter(funct, 1.52, -2,2,100)
+    px,py = psuedo_parameter(funct, 1.52 ,-2,2, 50)
+    
+    plt.plot(px,py,'.',label = 'Pseudo Arclength cont')
     plt.plot(y_true,x_true, label = 'Real' )
-    plt.plot(y,x, '.' ,label='Continuation')
+    #plt.plot(x,y, '.' ,label='Natural Continuation')
     plt.legend(loc = 'upper left')
     plt.show()
  
