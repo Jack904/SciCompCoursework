790eaa161d5bd872cdeabf0ccd40a98ae668a2e1
Jack <Jack904>
2023-04-19 19:07:22 +0100

    somethings almost working

--- Numerical_cont.py
+++ Numerical_cont.py
@@ -1,5 +1,5 @@
 import scipy
-from Predator_prey import shooting
+from Predator_prey import *
 import numpy as np
 import matplotlib.pyplot as plt
 import pytest # Look into this
@@ -10,76 +10,122 @@ def funct(t,x,c):
     return x**3 -x + c
 
 
-def natural_parameter(ode, initial_point, p0, p1, no_of_steps):
+def natural_parameter(ode, initial_point, p0, p1, no_of_steps, discretisation = []):
     x0 = [p0,*initial_point]
     
     x_vals = [initial_point]
     c_vals = [p0]
     h = (abs(p0-p1))/no_of_steps
+    
     output_check = ode(0,initial_point,p0)
-    for i in range(no_of_steps):
-        x0[0] +=  h
-        # x0[-1]-=0.25
-        if len(output_check) == 1:
-            myfunc = lambda u: np.concatenate(([ode(0,u[1:],u[0])],[u[0]-x0[0]]))
-        else:
-            myfunc = lambda u: np.concatenate(([ode(0,u[1:],u[0])[0]],[ode(0,u[1:],u[0])[1]],[u[0]-x0[0]]))
-        result = scipy.optimize.root(myfunc, x0 = x0, tol = 1e-10)
-        x0[-1] = result.x[1:]
-        
-
-        if result.success == True:
+    if discretisation == []:
+        for i in range(no_of_steps):
+            x0[0] +=  h
+            # x0[-1]-=0.25
+            if len(output_check) == 1:
+                myfunc = lambda u: np.concatenate(([ode(0,u[1:],u[0])],[u[0]-x0[0]]))
+            else:
+                myfunc = lambda u: np.concatenate(([ode(0,u[1:],u[0])[0]],[ode(0,u[1:],u[0])[1]],[u[0]-x0[0]]))
             
-            x_vals.append(result.x[1:])
-            c_vals.append(result.x[0])
+            
+            result = scipy.optimize.root(myfunc, x0 = x0, tol = 1e-10)
+            
+            x0[-1] = result.x[1:]
+            
+
+            if result.success == True:
+                
+                x_vals.append(result.x[1:])
+                c_vals.append(result.x[0])
+    elif discretisation == 'shooting':
+        for i in range(no_of_steps):
+            x0[0] +=  h
+            # x0[-1]-=0.25
+            myfunc = lambda u: np.concatenate(([shooting(u[1:],ode,[u[0]])[0]],[ode(0,u[1:3],u[0])[0]],[ode(0,u[1:3],u[0])[1]],[u[0]-x0[0]]))
+            
+            
+            result = scipy.optimize.root(myfunc, x0 = x0, tol = 1e-10)
+            x0[-1] = result.x[1:]
+            
+
+            if result.success == True:
+                
+                x_vals.append(result.x[1:])
+                c_vals.append(result.x[0])
     return [x_vals, c_vals]
 
 def psuedo_parameter(ode, initial_point,p0,p1, no_of_steps, discretisation = lambda x: x):
     h = (abs(p0-p1))/no_of_steps
-    if discretisation == 'shooting':
-        initial_guess = [*initial_point , 30]
-        result = scipy.optimize.root(shooting, x0 = initial_guess, args= (ode, p0))
-        xi_minus_one = [p0, *result.x[:-1]]
-    else: 
-        xi_minus_one = [p0,initial_point]
-    [y1, c1]  = natural_parameter(ode, xi_minus_one[1:],p0,p0+h,1)
-    xi = [p0+h, *y1[1]]
-    x_vals = np.zeros((len(initial_point), no_of_steps+1))
+    
+    xi_minus_one = [p0,*initial_point[0:2]]
+    [y1, c1]  = natural_parameter(ode, initial_point,p0,p0+h,1,'shooting')
+
+    xi = [p0+h, *y1[1][0:2]]
+    time_period = y1[1][2]
+    x_vals = np.zeros((len(initial_point)-1, no_of_steps+1))
     x_vals[0,0] = xi_minus_one[1]
     x_vals[1,0] = xi_minus_one[2]
     x_vals[0,1] = [*y1[1]][0]
     x_vals[1,1] = [*y1[1]][1]
     c_vals = [p0, p0+h]
     secant = np.zeros(len(xi_minus_one))
+
     output_check = ode(0,initial_point,p0)
     i =0
-    while xi[0] < p1:
-        i += 1
-        for j in range(len(initial_point)):
-            secant[j] = xi[j] - xi_minus_one[j]
+    if discretisation == []:
+        while xi[0] < p1:
+            i += 1
+            for j in range(len(initial_point)):
+                secant[j] = xi[j] - xi_minus_one[j]
 
-        prediction = xi + secant
-        if output_check == 1:
-            myfunc = lambda u: np.concatenate(([ode(0,u[1:],u[0])], [np.dot((u-prediction),secant)]))
-        else:
-            myfunc = lambda u: np.concatenate(([ode(0,u[1:],u[0])[0]], [ode(0,u[1:],u[0])[1]], [np.dot((u-prediction),secant)]))
-        result = scipy.optimize.root(myfunc, x0 = prediction)
-        xi_minus_one = xi
-        xi = result.x
-        
-        if result.success == True:
-            x_vals[0,i+1] = result.x[1]
-            x_vals[1,i+1] = result.x[2]
-            # x_vals.append(result.x[1:])
-            c_vals.append(result.x[0])
-            print(x_vals[0,i+1])
-            print(x_vals[1,i+1])
-        if i > no_of_steps:
-            break
+            prediction = xi + secant
+            if output_check == 1:
+                myfunc = lambda u: np.concatenate(([ode(0,u[1:],u[0])],[np.dot((u-prediction),secant)]))
+            else:
+                myfunc = lambda u: np.concatenate(([ode(0,u[1:],u[0])[0]],[ode(0,u[1:],u[0])[1]],[np.dot((u-prediction),secant)]))
+            result = scipy.optimize.root(myfunc, x0 = prediction, tol = 1e-10)
+            
+            xi_minus_one = xi
+            xi = result.x
+            
+            if result.success == True:
+                x_vals[0,i+1] = result.x[1]
+                x_vals[1,i+1] = result.x[2]
+                # x_vals.append(result.x[1:])
+                c_vals.append(result.x[0])
+            
+            if i > no_of_steps:
+                break
+    elif discretisation == 'shooting':
+        while xi[0] < p1:
+            i += 1
+            for j in range(len(initial_point)-1):
+                secant[j] = xi[j] - xi_minus_one[j]
+            prediction = xi + secant
+            if output_check == 1:
+                myfunc = lambda u: np.concatenate(([shooting(u[1:],ode,[u[0]])[0]],[ode(0,u[1:-1],u[0])],[np.dot((u[0:-1]-prediction),secant)]))
+            else:
+                myfunc = lambda u: np.concatenate(([shooting(u[1:],ode,[u[0]])[0]],[ode(0,u[1:-1],u[0])[0]],[ode(0,u[1:-1],u[0])[1]],[np.dot((u[0:-1]-prediction),secant)]))
+            
+            result = scipy.optimize.root(myfunc, x0 = [*prediction,time_period])
+            
+            xi_minus_one = xi
+            xi = result.x[0:3]
+            time_period = result.x[3]
+
+            if result.success == True:
+                x_vals[0,i+1] = result.x[1]
+                x_vals[1,i+1] = result.x[2]
+                # x_vals.append(result.x[1:])
+                c_vals.append(result.x[0])
+            
+            if i > no_of_steps:
+                break
+            prediction = xi + secant
     return [c_vals, x_vals]
 
 
-def ode(t,y,b): #Keeping t in in case our ode reuires it
+def hopf_ode(t,y,b): #Keeping t in in case our ode reuires it
     dx_dt = b*y[0] -y[1] -y[0]*((y[0])**2 +(y[1])**2)
     dy_dt = b*y[1] +y[0] -y[1]*((y[0])**2 +(y[1])**2)
 
@@ -91,7 +137,6 @@ def ode(t,y,b): #Keeping t in in case our ode reuires it
 
 
 if __name__ == "__main__":
-
     # y,x = natural_parameter(funct, 1.521, -2,2,100)
     # px,py = psuedo_parameter(funct, 1.521 ,-2,3, 200)
     # initial_guess = [0.8, 0.2,30]
@@ -99,22 +144,23 @@ if __name__ == "__main__":
     # b =0.1
     # d =0.1
     # condish = [a,b,d]
-    y,x = psuedo_parameter(ode, (0.06,0.06), 0,1,1000, discretisation='shooting')
+    y,x = psuedo_parameter(hopf_ode, (0.006,0.006,0.5), 0,2,1000, discretisation='shooting')
+    # plt.plot(x[0][0:29],y,'o', label = 'Y1')
+    plt.plot(y,x[1][0:len(y)],'o',label = 'Y2')
     
-    plt.plot(y,x[0][0:498],'.',label = 'Y1')
-    plt.plot(y,x[1][0:498],'.',label = 'Y2')
+    # plt.legend()
+    # plt.show()
 
     # # plt.plot(y_true,x_true, label = 'Real' )
     # plt.plot(x,y, '.' ,label='Natural Continuation')
-    plt.legend(loc = 'upper left')
-    plt.show()
+    
 
-
+    
 
 
     # Plotting Our hopf Bifurcation and checking if shooting works with it
-    # predator = scipy.integrate.solve_ivp(ode,[-10, 100],[0.006,0.006],args = [1],rtol = 1e-8)
-    # plt.plot(predator.t,predator.y[0,:], label = 'U1')
-    # plt.plot(predator.t,predator.y[1,:], label = 'U2')
-    # plt.show()
+    predator = scipy.integrate.solve_ivp(hopf_ode,[-10, 20],[0.006,0.006],args = [1],rtol = 1e-8)
+    plt.plot(predator.t,predator.y[0,:], label = 'U1')
+    plt.plot(predator.t,predator.y[1,:], label = 'U2')
+    plt.show()
 
--- Predator_prey.py
+++ Predator_prey.py
@@ -10,12 +10,21 @@ def ode(t,y,a,b,d): #Keeping t in in case our ode reuires it
 
 
 
+
 def shooting(x,F,conds=[]): #Finds the points and time period of the limit cycle
-    condition_1 = x[:len(x)-1] - scipy.integrate.solve_ivp(F,[0, x[len(x)-1]],x[:len(x)-1], args=(conds,), rtol = 1e-6).y[:,-1]
-    condition_2 = F(0,x[:len(x)-1],conds)[0] 
+    if conds == []:
+        condition_1 = x[:len(x)-1] - scipy.integrate.solve_ivp(F,[0, x[len(x)-1]],x[:len(x)-1], rtol = 1e-6).y[:,-1]
+        condition_2 = F(0,x[:len(x)-1])[0]
+    else:
+        
+        condition_1 = x[:len(x)-1] - scipy.integrate.solve_ivp(F,[0, x[len(x)-1]],x[:len(x)-1], args=(*conds,), rtol = 1e-6).y[:,-1]
+        condition_2 = F(0,x[:len(x)-1],*conds)[0] 
     
     return [*condition_1,condition_2] # THe * gets rid of the list within
-
+def shooting_solve(ode, initial_guess ,conds = []):
+    
+    result = scipy.optimize.root(shooting, x0 = initial_guess, args=(ode, conds)).x
+    return result
 
 
 if __name__ == "__main__":
@@ -24,16 +33,16 @@ if __name__ == "__main__":
     d =0.1
     condish = [a,b,d]
     # SOlving the ode
-    predator = scipy.integrate.solve_ivp(ode,[0, 100],[0.5,0.5],args=condish, rtol = 1e-4)
+    # predator = scipy.integrate.solve_ivp(ode,[0, 100],[0.5,0.5],args=condish, rtol = 1e-4)
     #Plotting ODE
-
     initial_guess = [0.8, 0.15,34]
-    result = scipy.optimize.root(shooting, x0 = initial_guess, args=(ode, condish))
-
-    print(result)
-    plt.plot(predator.t,predator.y[0,:], label = 'X')
-    plt.plot(predator.t,predator.y[1,:], label = 'y')
-    plt.plot(predator.t[38],result.x[1],marker = 'o')
-    plt.plot(predator.t[38],result.x[0],marker = 'o')
-    plt.legend()
-    plt.show()
+    # result = scipy.optimize.root(shooting, x0 = initial_guess, args=(ode, condish))
+    result = shooting_solve(ode, initial_guess, condish)
+    # result = shooting_solve(hopf_ode,[0.06, 0.06, 30],[0])
+   
+    # plt.plot(predator.t,predator.y[0,:], label = 'X')
+    # plt.plot(predator.t,predator.y[1,:], label = 'y')
+    # plt.plot(predator.t[38],result.x[1],marker = 'o')
+    # plt.plot(predator.t[38],result.x[0],marker = 'o')
+    # plt.legend()
+    # plt.show()
